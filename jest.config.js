e2fd734b5b7bced08643dd893649f02f34591dbf
NakaokaGabriel <gabrielnakaoka_99@hotmail.com>
2024-06-22 18:01:14 -0300

    chore: :wrench: config files to accept tests and config typescript

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  moduleDirectories: [
+    "src",
+    "node_modules"
+  ]
 };
